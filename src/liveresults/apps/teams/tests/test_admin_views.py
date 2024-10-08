8c3270bba92c29a35712ad880904d7f928fb5c71
rezah65 <HOSSEINI.REZA65@GMAIL.COM>
2024-10-08 10:04:03 +0330

    Add Honor admin View Test.

--- src/liveresults/apps/teams/tests/test_admin_views.py
+++ src/liveresults/apps/teams/tests/test_admin_views.py
@@ -4,7 +4,7 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 
 from liveresults.apps.leagues.models import League
-from liveresults.apps.teams.models import Team
+from liveresults.apps.teams.models import Team, Honor
 
 
 class TeamAdminViewTest(APITestCase):
@@ -58,3 +58,61 @@ class TeamAdminViewTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Team.objects.count(), 0)
 
+
+class HonorAdminTest(APITestCase):
+    def setUp(self):
+        self.league = League.objects.create(name='Premier League')
+        self.team = Team.objects.create(
+            league=self.league,
+            name='Test Team',
+            slug='test-team',
+            city='Test City',
+            established_date='2000-01-01'
+        )
+        self.honor = Honor.objects.create(
+            team=self.team,
+            title='Honor Test',
+            slug='honor-test',
+            year='2024-02-02',
+            description='description honor test',
+            level=Honor.LevelTextChoice.DOMESTIC
+
+        )
+
+    def test_create_honor(self):
+        url = reverse('admin-team:create-honor')
+        data = {
+            'team':self.team.name,
+            'title':'New Honor',
+            'slug':'new-honor',
+            'year':'2024-01-01',
+            'description':'new honor description'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_honor(self):
+        url = reverse('admin-team:detail-honor', args=[self.honor.pk])
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Honor Test')
+
+    def test_update_honor(self):
+        url = reverse('admin-team:detail-honor', args=[self.honor.pk])
+        data = {
+            'team':self.team.name,
+            'title':'Update Honor',
+            'slug':'update-honor',
+            'year':'2024-03-03',
+            'description':'update description'
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.honor.refresh_from_db()
+        self.assertEqual(self.honor.title, 'Update Honor')
+
+    def test_delete_honor(self):
+        url = reverse('admin-team:detail-honor', args=[self.honor.pk])
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Honor.objects.count(), 0)
